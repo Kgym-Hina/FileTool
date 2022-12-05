1e7435c453cdf14aba0d67d6ec4ac30746e49dd4
Hina <919835909@qq.com>
2022-12-05 18:53:06 +0800

    搓好了

---
+++ FileTool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine($"源文件夹 {args[0]}");
+            Console.Out.WriteLine($"目标文件夹为 /当前目录/Out/");
+
+            // Loop to get all file's path in all folders including subfolders
+            var files = Directory.GetFiles(args[0], "*.*", SearchOption.AllDirectories);
+            Directory.CreateDirectory($"{args[0]}/Out");
+
+            
+            foreach (var file in files)
+            {
+                if (file.Split("/").Last().StartsWith("."))
+                {
+                    continue;
+                }
+                Console.Out.WriteLine($"{file}");
+                File.Copy(file, $"{args[0]}/Out/{file.Split("/").Last()}");
+            }
+        }
+    }
+}
